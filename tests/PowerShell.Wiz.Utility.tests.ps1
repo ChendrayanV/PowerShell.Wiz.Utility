75a215a57a885a6a33850f8b092b9b44758d22cf
Chendrayan Venkatesan <venkatesan.chendrayan@gmail.com>
2024-07-06 16:24:03 +0100

    new version 0.1.0

--- tests/PowerShell.Wiz.Utility.tests.ps1
+++ tests/PowerShell.Wiz.Utility.tests.ps1
@@ -4,12 +4,20 @@ param (
     $ClientSecret
 )
 
-Import-Module "$PSScriptRoot\..\PowerShell.Wiz.Utility.psd1" -Verbose -Force
+Import-Module "$PSScriptRoot\..\PowerShell.Wiz.Utility.psd1" -Force
 
 
 Describe "PowerShell.Wiz.Utilty" {
     
-    It "Wiz Authentication" {
-        Get-PSWizAuthenticationToken -ClientID $($ClientID) -ClientSecret $($ClientSecret) | Should -BeNullOrEmpty
+    Context 'Authenitcation' {
+        It "Wiz Authentication" {
+            Get-PSWizAuthenticationToken -ClientID $($ClientID) -ClientSecret $($ClientSecret) | Should -BeNullOrEmpty
+        }
+    }
+
+    Context 'Retrieve Wiz Project' {
+        It "List Wiz Project - All" {
+            Get-PSWizProject | Should -BeOfType [System.Management.Automation.PSCustomObject]
+        }
     }
 }
