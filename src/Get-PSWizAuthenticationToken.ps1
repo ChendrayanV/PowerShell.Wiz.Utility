b82864e00ebd8f1f2e6d0d0d0b3ccaeaa7b73525
Chendrayan Venkatesan <venkatesan.chendrayan@gmail.com>
2024-07-06 14:14:08 +0100

    developing test cases

--- src/Get-PSWizAuthenticationToken.ps1
+++ src/Get-PSWizAuthenticationToken.ps1
@@ -47,14 +47,20 @@ function Get-PSWizAuthenticationToken {
         client_secret = $ClientSecret
         audience      = 'wiz-api'
     }
-    $response = Invoke-RestMethod 'https://auth.app.wiz.io/oauth/token' -Method POST -ContentType 'application/x-www-form-urlencoded'  -Body $body 
-    $access_token = $response.access_token
+    try {
+        $response = Invoke-RestMethod 'https://auth.app.wiz.io/oauth/token' -Method POST -ContentType 'application/x-www-form-urlencoded'  -Body $body 
+        $access_token = $response.access_token
+        $tokenPayload = $access_token.Split(".")[1].Replace('-', '+').Replace('_', '/')
+        while ($tokenPayload.Length % 4) { Write-Verbose "Invalid length for a Base-64 char array or string, adding ="; $tokenPayload += "=" }
+        $tokenByteArray = [System.Convert]::FromBase64String($tokenPayload)
+        $tokenArray = [System.Text.Encoding]::ASCII.GetString($tokenByteArray)
+        $tokobj = $tokenArray | ConvertFrom-Json
+        $Script:Access_Token = $access_token
+        $Script:Data_Center = $tokobj.dc
+    }
+    catch {
+        Write-Error -Exception $_.Exception.Message -InformationAction Continue
+    }
 
-    $tokenPayload = $access_token.Split(".")[1].Replace('-', '+').Replace('_', '/')
-    while ($tokenPayload.Length % 4) { Write-Verbose "Invalid length for a Base-64 char array or string, adding ="; $tokenPayload += "=" }
-    $tokenByteArray = [System.Convert]::FromBase64String($tokenPayload)
-    $tokenArray = [System.Text.Encoding]::ASCII.GetString($tokenByteArray)
-    $tokobj = $tokenArray | ConvertFrom-Json
-    $Script:Access_Token = $access_token
-    $Script:Data_Center = $tokobj.dc
+    
 }
--- tests/PowerShell.Wiz.Utility.tests.ps1
+++ tests/PowerShell.Wiz.Utility.tests.ps1
@@ -1,8 +1,13 @@
+param (
+    $Param1
+)
+
 Import-Module "$PSScriptRoot\..\PowerShell.Wiz.Utility.psd1" -Verbose -Force
+
 
 Describe "PowerShell.Wiz.Utilty" {
     
     It "dummt test" {
-        1 | Should -BeExactly 1
+        $Param1 | Should -BeExactly 1
     }
 }
